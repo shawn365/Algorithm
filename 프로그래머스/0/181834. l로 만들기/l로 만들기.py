8debdb89932f4b4c3f1e04adaaf00c9cb6f6c044
shawn365 <84111392+shawn365@users.noreply.github.com>
2025-02-13 13:09:44 +0900

    [level 0] Title: l로 만들기, Time: 14.32 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,9 @@
+def solution(myString):
+    answer = ''
+    for i in myString:
+        if "l" > i:
+            answer += "l"
+        else:
+            answer += i
+            
+    return answer
